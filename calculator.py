ac10d382d2c52d812462ea680bcfecc6affc63e9
Lucy Ivatt <lucy.ivatt@xdesign.com>
2024-07-12 22:00:04 +0100

    added power function

--- calculator.py
+++ calculator.py
@@ -14,7 +14,12 @@ def divide(a, b):
     return a // b
 
 
+def power(a, b):
+    return a ** b
+
+
 print("5 + 3 =", add(5, 3))
 print("4 * 6 =", multiply(4, 6))
 print("10 - 2 =", subtract(10, 2))
 print("21 / 3 =", divide(21, 3))
+print("2^2 =", power(2, 2))
