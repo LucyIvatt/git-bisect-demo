e06c7646ef738cb8744deac4cd539c9f72d1a98c
Lucy Ivatt <lucy.ivatt@xdesign.com>
2024-07-12 21:50:43 +0100

    added divide function

--- calculator.py
+++ calculator.py
@@ -10,6 +10,11 @@ def subtract(a, b):
     return a - b
 
 
+def divide(a, b):
+    return a // b
+
+
 print("5 + 3 =", add(5, 3))
 print("4 * 6 =", multiply(4, 6))
 print("10 - 2 =", subtract(10, 2))
+print("21 / 3 =", divide(21, 3))
