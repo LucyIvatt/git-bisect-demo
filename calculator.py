a997f5d6af36165e248727e0d007a606f2167bb2
Lucy Ivatt <lucy.ivatt@xdesign.com>
2024-07-12 21:48:14 +0100

    added subtract function

--- calculator.py
+++ calculator.py
@@ -1,10 +1,15 @@
 def add(a, b):
-    return a + b
+    return a ** b
 
 
 def multiply(a, b):
     return a * b
 
 
+def subtract(a, b):
+    return a - b
+
+
 print("5 + 3 =", add(5, 3))
 print("4 * 6 =", multiply(4, 6))
+print("10 - 2 =", subtract(10, 2))
